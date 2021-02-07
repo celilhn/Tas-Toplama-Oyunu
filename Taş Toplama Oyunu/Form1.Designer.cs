13fc2e3acdcc498a13778b593eec4a5a0a77aca7
Celilhan <celilhan12363@gmail.com>
2021-02-07 20:29:49 +0300

    Add files via upload

---
+++ Taş Toplama Oyunu/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace Taş_Toplama_Oyunu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lblStok = new System.Windows.Forms.Label();
+            this.lbl_ana_tas = new System.Windows.Forms.Label();
+            this.lbl_oyuncu1_yantas = new System.Windows.Forms.Label();
+            this.lbl_duvaroyuncu1 = new System.Windows.Forms.Label();
+            this.lbl_duvar_oyuncu2 = new System.Windows.Forms.Label();
+            this.lbl_oyuncu2_yantas = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lbl_Oyuncu_Siralama = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbl_stok_2 = new System.Windows.Forms.Label();
+            this.lbl_level = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblStok
+            // 
+            this.lblStok.AutoSize = true;
+            this.lblStok.Location = new System.Drawing.Point(130, 486);
+            this.lblStok.Name = "lblStok";
+            this.lblStok.Size = new System.Drawing.Size(0, 13);
+            this.lblStok.TabIndex = 184;
+            // 
+            // lbl_ana_tas
+            // 
+            this.lbl_ana_tas.AllowDrop = true;
+            this.lbl_ana_tas.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lbl_ana_tas.Image = ((System.Drawing.Image)(resources.GetObject("lbl_ana_tas.Image")));
+            this.lbl_ana_tas.Location = new System.Drawing.Point(439, 246);
+            this.lbl_ana_tas.Name = "lbl_ana_tas";
+            this.lbl_ana_tas.Size = new System.Drawing.Size(40, 40);
+            this.lbl_ana_tas.TabIndex = 187;
+            this.lbl_ana_tas.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lbl_ana_tas.MouseDown += new System.Windows.Forms.MouseEventHandler(this.lbl_ana_tas_MouseDown);
+            // 
+            // lbl_oyuncu1_yantas
+            // 
+            this.lbl_oyuncu1_yantas.AllowDrop = true;
+            this.lbl_oyuncu1_yantas.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lbl_oyuncu1_yantas.Image = ((System.Drawing.Image)(resources.GetObject("lbl_oyuncu1_yantas.Image")));
+            this.lbl_oyuncu1_yantas.Location = new System.Drawing.Point(108, 499);
+            this.lbl_oyuncu1_yantas.Name = "lbl_oyuncu1_yantas";
+            this.lbl_oyuncu1_yantas.Size = new System.Drawing.Size(40, 40);
+            this.lbl_oyuncu1_yantas.TabIndex = 103;
+            this.lbl_oyuncu1_yantas.MouseDown += new System.Windows.Forms.MouseEventHandler(this.lbl_tas_MouseDown);
+            // 
+            // lbl_duvaroyuncu1
+            // 
+            this.lbl_duvaroyuncu1.AllowDrop = true;
+            this.lbl_duvaroyuncu1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lbl_duvaroyuncu1.ForeColor = System.Drawing.SystemColors.Control;
+            this.lbl_duvaroyuncu1.Image = ((System.Drawing.Image)(resources.GetObject("lbl_duvaroyuncu1.Image")));
+            this.lbl_duvaroyuncu1.Location = new System.Drawing.Point(154, 499);
+            this.lbl_duvaroyuncu1.Name = "lbl_duvaroyuncu1";
+            this.lbl_duvaroyuncu1.Size = new System.Drawing.Size(40, 40);
+            this.lbl_duvaroyuncu1.TabIndex = 188;
+            this.lbl_duvaroyuncu1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lbl_duvaroyuncu1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.lbl_duvar_MouseDown_1);
+            // 
+            // lbl_duvar_oyuncu2
+            // 
+            this.lbl_duvar_oyuncu2.AllowDrop = true;
+            this.lbl_duvar_oyuncu2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lbl_duvar_oyuncu2.ForeColor = System.Drawing.SystemColors.Control;
+            this.lbl_duvar_oyuncu2.Image = ((System.Drawing.Image)(resources.GetObject("lbl_duvar_oyuncu2.Image")));
+            this.lbl_duvar_oyuncu2.Location = new System.Drawing.Point(154, 35);
+            this.lbl_duvar_oyuncu2.Name = "lbl_duvar_oyuncu2";
+            this.lbl_duvar_oyuncu2.Size = new System.Drawing.Size(40, 40);
+            this.lbl_duvar_oyuncu2.TabIndex = 191;
+            this.lbl_duvar_oyuncu2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lbl_duvar_oyuncu2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.lbl_duvar_2_MouseDown);
+            // 
+            // lbl_oyuncu2_yantas
+            // 
+            this.lbl_oyuncu2_yantas.AllowDrop = true;
+            this.lbl_oyuncu2_yantas.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lbl_oyuncu2_yantas.Image = ((System.Drawing.Image)(resources.GetObject("lbl_oyuncu2_yantas.Image")));
+            this.lbl_oyuncu2_yantas.Location = new System.Drawing.Point(108, 35);
+            this.lbl_oyuncu2_yantas.Name = "lbl_oyuncu2_yantas";
+            this.lbl_oyuncu2_yantas.Size = new System.Drawing.Size(40, 40);
+            this.lbl_oyuncu2_yantas.TabIndex = 189;
+            this.lbl_oyuncu2_yantas.MouseDown += new System.Windows.Forms.MouseEventHandler(this.lbl_tas_2_MouseDown);
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.PeachPuff;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(12, 44);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(90, 23);
+            this.label4.TabIndex = 192;
+            this.label4.Text = "Oyuncu 2";
+            // 
+            // lbl_Oyuncu_Siralama
+            // 
+            this.lbl_Oyuncu_Siralama.AutoSize = true;
+            this.lbl_Oyuncu_Siralama.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl_Oyuncu_Siralama.Location = new System.Drawing.Point(277, 508);
+            this.lbl_Oyuncu_Siralama.Name = "lbl_Oyuncu_Siralama";
+            this.lbl_Oyuncu_Siralama.Size = new System.Drawing.Size(0, 18);
+            this.lbl_Oyuncu_Siralama.TabIndex = 193;
+            // 
+            // label6
+            // 
+            this.label6.BackColor = System.Drawing.Color.PeachPuff;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(12, 508);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(90, 22);
+            this.label6.TabIndex = 194;
+            this.label6.Text = "Oyuncu 1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(430, 222);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 195;
+            this.label1.Text = "ANA TAŞ";
+            // 
+            // lbl_stok_2
+            // 
+            this.lbl_stok_2.AutoSize = true;
+            this.lbl_stok_2.Location = new System.Drawing.Point(118, 13);
+            this.lbl_stok_2.Name = "lbl_stok_2";
+            this.lbl_stok_2.Size = new System.Drawing.Size(0, 13);
+            this.lbl_stok_2.TabIndex = 196;
+            // 
+            // lbl_level
+            // 
+            this.lbl_level.AutoSize = true;
+            this.lbl_level.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl_level.ForeColor = System.Drawing.Color.Red;
+            this.lbl_level.Location = new System.Drawing.Point(395, 51);
+            this.lbl_level.Name = "lbl_level";
+            this.lbl_level.Size = new System.Drawing.Size(80, 24);
+            this.lbl_level.TabIndex = 197;
+            this.lbl_level.Text = "LEVEL ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PeachPuff;
+            this.ClientSize = new System.Drawing.Size(499, 560);
+            this.Controls.Add(this.lbl_level);
+            this.Controls.Add(this.lbl_stok_2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.lbl_Oyuncu_Siralama);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lbl_duvar_oyuncu2);
+            this.Controls.Add(this.lbl_oyuncu2_yantas);
+            this.Controls.Add(this.lbl_duvaroyuncu1);
+            this.Controls.Add(this.lbl_ana_tas);
+            this.Controls.Add(this.lblStok);
+            this.Controls.Add(this.lbl_oyuncu1_yantas);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lbl_oyuncu1_yantas;
+        private System.Windows.Forms.Label lblStok;
+        private System.Windows.Forms.Label lbl_ana_tas;
+        private System.Windows.Forms.Label lbl_duvaroyuncu1;
+        private System.Windows.Forms.Label lbl_duvar_oyuncu2;
+        private System.Windows.Forms.Label lbl_oyuncu2_yantas;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lbl_Oyuncu_Siralama;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbl_stok_2;
+        private System.Windows.Forms.Label lbl_level;
+    }
+}
+
---
+++ Taş Toplama Oyunu/Form1.cs
@@ -0,0 +1,348 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Taş_Toplama_Oyunu
+{
+    public partial class Form1 : Form
+    {
+        // Değişkenler
+        private int birinciOyuncu_tasStok = 3;
+        private int ikinciOyuncu_tasStok = 3;
+        private int ana_tas_stok = 1;
+        private string[,] label_matris = new string[10, 10];
+        private Label temp_ana_lbl;
+        private Label[] lbl_duvarlar = new Label[15];
+        private Label[] lDizi = new Label[100];
+        private int counter_duvar = 0;
+        private bool oyuncu1 = true;
+        private bool oyuncu2 = false;
+        private bool anatas = false;
+        private int counterLevel = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.AllowDrop = true;
+
+        }
+        
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            doldur(); // Label matrisi oluşturur
+            lbl_Oyuncu_Siralama.Text = "Lütfen anataşı yerleştiriniz.";
+            lbl_level.Text = "LEVEL 1";
+        }
+
+        // 10*10 Label matrisi oluşturur ve konumlandırır. Ulaşabilmek için sıralı şekilde dizi içerisine atandı.
+        private void doldur()
+        {
+            bool IsColor = true;
+            int counter = 0;
+
+            lblStok.Text = birinciOyuncu_tasStok.ToString();
+            lbl_stok_2.Text = ikinciOyuncu_tasStok.ToString();
+
+            for (int i = 0; i < 10; i++) // Matris oluşturuluyor
+            {
+
+                for (int j = 0; j < 10; j++) 
+                {
+                    // Label oluşturma
+                    lDizi[counter] = new Label();
+
+                    lDizi[counter].Name = "lbl" + counter;
+
+                    label_matris[i, j] = lDizi[counter].Name;
+
+                    this.Controls.Add(lDizi[counter]);
+
+                    lDizi[counter].Top = 80 + (i * 40);
+
+                    lDizi[counter].Left = 20 + (40 * j);
+
+                    if (IsColor) // Beyaz renklendirme
+                    {
+                        lDizi[counter].BackColor = Color.White;
+                        IsColor = false;
+                    }
+                    else // Siyah renklendirme
+                    {
+                        lDizi[counter].BackColor = Color.Black;
+                        IsColor = true;
+                    }
+                    lDizi[counter].Visible = true; // Görünürlük
+                    lDizi[counter].AutoSize = false; 
+                    lDizi[counter].AllowDrop = true; // Üstüne sürüklenen nesneyi almasını sağlar
+                    lDizi[counter].Width = 40;
+                    lDizi[counter].Height = 40;
+
+                    // Label sürükle bırak metodları tanımlandı. Fonksiyonlar atandı.
+                    lDizi[counter].DragDrop += new System.Windows.Forms.DragEventHandler(label_DragDrop);
+                    lDizi[counter].DragEnter += new System.Windows.Forms.DragEventHandler(label_DragEnter);
+
+                    counter++;
+                }
+
+                //  Renklendirme ayarları
+                if (IsColor)
+                    IsColor = false;
+                else
+                    IsColor = true;
+            }
+        }
+
+        private void label_DragDrop(object sender, DragEventArgs e) // Sürükle bırak metodu
+        {
+            try
+            {
+                Label L = (Label)sender;
+
+                if (L.Image == null) // İmage yok ise
+                {
+                    if (e.Data.GetData(DataFormats.Bitmap) == lbl_oyuncu1_yantas.Image && birinciOyuncu_tasStok > 0) // 1. oyuncu taşı sürüklendiyse ve yan taş stoğu var ise
+                    {
+                        if (oyuncu1 && anatas) // Sıra 1. oyuncuda ise ve anataş matrise eklendi ise 
+                        {
+                            // Matrise eklenen taşlar lbl_duvarlar dizisine atanıyor
+                            lbl_duvarlar[counter_duvar] = L;
+                            counter_duvar++;
+
+                            // Label ayarları
+                            L.Image = lbl_oyuncu1_yantas.Image;
+                            L.Text = UzaklikHesapla(L).ToString(); // Uzaklık hesaplanıp text'e yazdırılıyor
+                            L.TextAlign = ContentAlignment.MiddleCenter;
+                            L.ForeColor = Color.Red;
+                            L.Font = new Font("Arial", 20, FontStyle.Bold);
+
+                            // Yan taş stoğu bir azaltılıyor ve lblStok'e yazdırılıyor
+                            if (birinciOyuncu_tasStok > 0)
+                                birinciOyuncu_tasStok--;
+                            lblStok.Text = birinciOyuncu_tasStok.ToString();
+
+                            // Sırası gelen oyuncu lbl_Oyuncu_Siralama'a yazdırılıyor ve oyuncu sıra değişikliği yapılıyor
+                            lbl_Oyuncu_Siralama.Text = "Sıra 2. oyuncuda";
+                            oyuncu1 = false;
+                            oyuncu2 = true;
+                        }
+                        else if (anatas && !oyuncu1) // Anataş matrise eklendiyse ve sıra oyuncuda değilse, sıranın kimde olduğunu yazdırıyor
+                        {
+                            lbl_Oyuncu_Siralama.Text = "Sıra 2. oyuncuda";
+                        }
+                        else
+                            lbl_Oyuncu_Siralama.Text = "Lüfen anataşı yerleştiriniz.";
+                            
+                    }
+                    else if (e.Data.GetData(DataFormats.Bitmap) == lbl_ana_tas.Image && ana_tas_stok > 0) // Ana taş sürüklendiyse ve ana taş stoğu var ise
+                    {
+                        anatas = true; // Anataş matrise eklendi
+                        temp_ana_lbl = L; // Ana taş label'inin yedeği alınıyor
+
+                        // Label ayarları
+                        L.Image = lbl_ana_tas.Image;
+                        L.TextAlign = ContentAlignment.MiddleCenter;
+                        L.ForeColor = Color.Red;
+                        L.Font = new Font("Arial", 20, FontStyle.Bold);
+                        lbl_Oyuncu_Siralama.Text = "Sıra 1. Oyuncuda";
+
+                        // Ana taş stoğu bir azaltılıyor
+                        if (ana_tas_stok > 0)
+                            ana_tas_stok--;
+                    }
+                    else if (e.Data.GetData(DataFormats.Bitmap) == lbl_duvaroyuncu1.Image) // 1. duvar sürüklendiyse ve duvar stoğu var ise
+                    {
+                        // Matrise eklenen taşlar lbl_duvarlar dizisine atanıyor
+                        lbl_duvarlar[counter_duvar] = L; 
+                        counter_duvar++;
+
+                        // Label ayarları
+                        L.Image = lbl_duvaroyuncu1.Image;
+                        L.TextAlign = ContentAlignment.MiddleCenter;
+                        L.ForeColor = Color.Red;
+                        L.Font = new Font("Arial", 20, FontStyle.Bold);
+                    }
+                    if (e.Data.GetData(DataFormats.Bitmap) == lbl_oyuncu2_yantas.Image && ikinciOyuncu_tasStok > 0) // 2. oyuncu yan taşı sürüklendiyse ve yan taş stoğu var ise
+                    {
+                        if(oyuncu2 && anatas) // Sıra 2. oyuncuda ise ve anataş matrise eklendi ise 
+                        {
+                            // Matrise eklenen taşlar lbl_duvarlar dizisine atanıyor
+                            lbl_duvarlar[counter_duvar] = L;
+                            counter_duvar++;
+
+                            // Label ayarları
+                            L.Image = lbl_oyuncu1_yantas.Image;
+                            L.Text = UzaklikHesapla(L).ToString(); // Uzaklık hesaplanıp text'e yazdırılıyor
+                            L.TextAlign = ContentAlignment.MiddleCenter;
+                            L.ForeColor = Color.Red;
+                            L.Font = new Font("Arial", 20, FontStyle.Bold);
+
+                            // Yan taş stoğu bir azaltılıyor ve lbl_stok_2'e yazdırılıyor
+                            if (ikinciOyuncu_tasStok > 0)
+                                ikinciOyuncu_tasStok--;
+                            lbl_stok_2.Text = ikinciOyuncu_tasStok.ToString();
+
+                            // Sırası gelen oyuncu lbl_Oyuncu_Siralama'a yazdırılıyor ve oyuncu sıra değişikliği yapılıyor
+                            lbl_Oyuncu_Siralama.Text = "Sıra 1. oyuncuda";
+                            oyuncu2 = false;
+                            oyuncu1 = true;
+                        }
+                        else if(anatas && oyuncu2) // Anataş matrise eklendiyse ve sıra oyuncuda değilse, sıranın kimde olduğunu yazdırıyor
+                        {
+                            lbl_Oyuncu_Siralama.Text = "Sıra 1. oyuncuda";
+                        }
+                        else
+                            lbl_Oyuncu_Siralama.Text = "Lüfen anataşı yerleştiriniz.";
+                    }
+                    else if (e.Data.GetData(DataFormats.Bitmap) == lbl_duvar_oyuncu2.Image) // 2. oyuncu duvar sürüklendiyse ve duvar stoğu var ise
+                    {
+                        // Matrise eklenen taşlar lbl_duvarlar dizisine atanıyor
+                        lbl_duvarlar[counter_duvar] = L; 
+                        counter_duvar++;
+
+                        // Label ayarları
+                        L.Image = lbl_duvaroyuncu1.Image;
+                        L.TextAlign = ContentAlignment.MiddleCenter;
+                        L.ForeColor = Color.Red;
+                        L.Font = new Font("Arial", 20, FontStyle.Bold);
+                    }
+
+                    // Eğer bütün yantaşlar matrise eklendi ise matrise temizleniyor ve diğer bölüme geçiliyor
+                    if(birinciOyuncu_tasStok == 0 && ikinciOyuncu_tasStok == 0)
+                    {
+                        temizle();
+                        lbl_Oyuncu_Siralama.Text = "Lüfen anataşı yerleştiriniz.";
+                    }
+                }
+            }
+            catch(InvalidCastException a)
+            {
+                MessageBox.Show("Exception " + a);
+            }
+                
+        }
+
+        // Matris ve değişkenler temizleniyor ve diğer level ayarları yapılıyor
+        private void temizle()
+        {
+            counterLevel++; // Level sayısı
+
+            if (counterLevel == 1) // 2 levelli olacak
+            {
+                // Label resimleri ve textleri temizleniyor
+                for (int i = 0; i < 100; i++)
+                {
+                    lDizi[i].Image = null;
+                    lDizi[i].Text = "";
+                }
+
+                // Yeni bölüm taş stokları
+                ana_tas_stok = 1;
+                birinciOyuncu_tasStok = 4;
+                ikinciOyuncu_tasStok = 4;
+
+                // Label textlerş
+                lblStok.Text = birinciOyuncu_tasStok.ToString();
+                lbl_stok_2.Text = ikinciOyuncu_tasStok.ToString();
+
+                // Oyuncu ayarları
+                oyuncu1 = false;
+                oyuncu2 = true;
+                anatas = false;
+
+                // Değişkenler temizlendi
+                lbl_duvarlar = new Label[10];
+                temp_ana_lbl = new Label();
+
+                lbl_level.Text = "LEVEL 2";
+            }
+            else
+                this.Close();
+        }
+
+        private void label_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.Copy;
+        }
+
+        private void lbl_tas_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left && birinciOyuncu_tasStok > 0) // Mausenin sol butonuna basıldı ise ve yantaş stoğu 0 dan büyükse 
+            {
+                lbl_oyuncu1_yantas.DoDragDrop(lbl_oyuncu1_yantas.Image, DragDropEffects.All);
+            }
+            
+        }
+
+        private void lbl_ana_tas_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left && ana_tas_stok > 0) // Mausenin sol butonuna basıldı ise ve yantaş stoğu 0 dan büyükse 
+            {
+                lbl_ana_tas.DoDragDrop(lbl_ana_tas.Image, DragDropEffects.All);
+            }
+        }
+
+        private void lbl_duvar_MouseDown_1(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left) // Mausenin sol butonuna basıldı ise
+            {
+                lbl_duvaroyuncu1.DoDragDrop(lbl_duvaroyuncu1.Image, DragDropEffects.All);
+            }
+        }
+
+        // Ana taş ile yan taş arasındaki uzaklığı hesaplama
+        private int UzaklikHesapla(Label L)
+        {
+            // Yan taşın kordinat bilgileri
+            int tas_satir = L.Name[3] - 48;
+            int tas_sutun;
+            if (L.Name.Length == 4)
+                tas_sutun = 0;
+            else
+                tas_sutun = L.Name[4] - 48;
+
+            // Ana taşın kordinat bilgileri
+            int ana_tas_satir = temp_ana_lbl.Name[3] - 48;
+            int ana_tas_sutun;
+            if (temp_ana_lbl.Name.Length == 4)
+                ana_tas_sutun = 0;
+            else
+                ana_tas_sutun = temp_ana_lbl.Name[4] - 48;
+
+            // Matris uzaklık metoduna göre uzaklık hesaplandı.
+            int adımSayisi = Math.Abs(tas_satir - ana_tas_satir) + Math.Abs(tas_sutun - ana_tas_sutun);
+
+            return adımSayisi;
+        }
+
+        // LEVEL 2
+        private void btn_level_2_Click(object sender, EventArgs e)
+        {
+            //// Matris ve değişkenler temizleniyor ve diğer level ayarları yapılıyor
+            temizle();
+        }
+
+        private void lbl_tas_2_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)  // Mausenin sol butonuna basıldı ise
+            {
+                lbl_oyuncu1_yantas.DoDragDrop(lbl_oyuncu2_yantas.Image, DragDropEffects.All);
+            }
+        }
+
+        private void lbl_duvar_2_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)  // Mausenin sol butonuna basıldı ise
+            {
+                lbl_duvaroyuncu1.DoDragDrop(lbl_duvar_oyuncu2.Image, DragDropEffects.All);
+            }
+        }
+
+    }
+}
